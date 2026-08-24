a4a4fb60573fa9d1961557c41b8598c3542246a8
librimania <librimaniastore@gmail.com>
2026-08-24 19:11:59 +0300

    Add app/writing/bac/topics.ts

---
+++ app/writing/bac/topics.ts
@@ -0,0 +1,11 @@
+import { technologyEssays } from "./technology/essays";
+
+export const bacTopics = [
+  {
+    id: "technology",
+    title: "Technology",
+    route: "/writing/bac/technology",
+    subtopics: technologyEssays,
+  },
+] as const;
+
