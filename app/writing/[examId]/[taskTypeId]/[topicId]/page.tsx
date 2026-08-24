df0c663b79c8151adf592f87c3baeccd05bc6c76
librimania <librimaniastore@gmail.com>
2026-08-24 19:11:43 +0300

    Add app/writing/[examId]/[taskTypeId]/[topicId]/page.tsx

---
+++ app/writing/[examId]/[taskTypeId]/[topicId]/page.tsx
@@ -0,0 +1,3 @@
+import WritingBrowser from "../../../WritingBrowser";
+export default async function Page({params}:{params:Promise<{examId:string,taskTypeId:string,topicId:string}>}){const p=await params;return <WritingBrowser ids={[p.examId,p.taskTypeId,p.topicId]}/>}
+
